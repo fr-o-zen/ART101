9081e0014b65662d0d30207e397c0e3798e33dac
fr-o-zen <harrysant006@gmail.com>
2025-11-26 16:10:18 -0700

    Lab 9 adjustments to env04

--- environ/env04.js
+++ environ/env04.js
@@ -1,25 +1,40 @@
-bigTitle = "BEOWULF BEGINS";
-height = "6'7";
-weight = 287;
+let bigTitle = "BEOWULF BEGINS";
+let height = "6'7";
+let weight = 287;
 let beoLikes = ["folded steel", "The Hurting", "justice", "merchandise"];
 
-
 $("#titleoutput").html(bigTitle);
-statSheet = "height: " + height + " weight: " + weight 
-favorites = "Likes: " + beoLikes[0] + ", " + beoLikes [1] + ", " + beoLikes[2] + ", " + beoLikes[3]
+
+let statSheet = "height: " + height + " weight: " + weight;
+let favorites = "Likes: " + beoLikes.join(", ");
 
 $("#statoutput").html(statSheet);
 $("#likesoutput").html(favorites);
 
-//beowulfPoses ["beowulf_burst", "beowulf_victory","beowulf_punch"];
+// FIXED pose list
+let beowulfPoses = ["beowulf_burst", "beowulf_victory", "beowulf_punch"];
+let currentPoseIndex = 0;
 
-//function makeImage (imageName) {
-    //$("#beowulf-output").append("<img width=400 src='images/" + imageName + ".png>");
+// FIXED makeImage() – removes old image first
+function makeImage(imageName) {
+    $("#beowulf-output").empty();  // remove previous image
+    $("#beowulf-output").append(
+        "<img width='400' src='images/" + imageName + ".png'>"
+    );
+}
 
-//}
-
+// On button click, cycle through all poses
 $("#beo-pose1").click(function () {
-    imageName=="beowulf_burst";
-    $("#beowulf-output").append("<img width=400 src='images/" + imageName + ".png>");
 
+    // Get the current pose
+    let poseToShow = beowulfPoses[currentPoseIndex];
+
+    // Show it
+    makeImage(poseToShow);
+
+    // Move to next pose (loop back to 0 at end)
+    currentPoseIndex++;
+    if (currentPoseIndex >= beowulfPoses.length) {
+        currentPoseIndex = 0;
+    }
 });
